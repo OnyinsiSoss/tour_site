f771e9b44300c1bcf4fa0554cca21c5fafe293d9
Onyinsi <onyinsisoss@outlook.com>
2024-10-03 15:58:51 +0300

    .

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function top_menu() {
     if(document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
         document.getElementById('top-menu').style.top = '0';
     } else {
-        document.getElementById('top-menu').style.top = '-60px';
+        document.getElementById('top-menu').style.top = '-73px';
     }
 }
